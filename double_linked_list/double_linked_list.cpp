d7939cf7b8fa2841739ba36cdd27b541ac94d00b
defarrel <Defarrel>
2023-05-25 10:53:52 +0700

    Membuat if else untuk nomer yang sama

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class node {
 public:
-	int nomhs;
+	int noMhs;
 	string name;
 	node* next;
 	node* prev;
@@ -39,8 +39,21 @@ void DoubleLinkedList::addNode(){
 	cin.ignore();
 	getline(cin, nm);
 	node* newNode = new node();
-	newNode->nomhs = nim;
+	newNode->noMhs = nim;
 	newNode->name = nm;
 
-
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev - newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
+	
+	
 }
