d15c607e384b5fbb4ab60d375d5af27e6b0e1f1e
defarrel <Defarrel>
2023-05-25 10:30:12 +0700

    membuat method untuk input

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -30,3 +30,17 @@ DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
 
+void DoubleLinkedList::addNode(){
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin.ignore();
+	getline(cin, nm);
+	node* newNode = new node();
+	newNode->nomhs = nim;
+	newNode->name = nm;
+
+
+}
